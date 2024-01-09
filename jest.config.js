1e966d99b8495328643ff84a9a0c230241a397de
Daniel Zenzes <daniel.zenzes@adesso.de>
2024-01-09 07:09:37 +0100

    chore: updated dependencies and configs

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  roots: ["<rootDir>/src"],
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
-  ],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.d.ts",
-    "!**/node_modules/**",
-  ],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
-  },
+  preset: "ts-jest",
+  testEnvironment: "node",
 };
